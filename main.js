e7699b600a9443d1d10a44ebfa2decef343773f3
Nicholas Farley <nicholas_farley@colpal.com>
2023-11-09 13:59:17 -0500

    Show waves sorted by size, preserving index

--- main.js
+++ main.js
@@ -10,5 +10,10 @@ const text = await Deno.readTextFile(Deno.args[0]);
 const edges = [...parseEdges(text)];
 const forwards = groupForwards(edges);
 const backwards = groupBackwards(edges);
-const waves = [...generateWaves("[root] root", backwards, forwards)];
-console.log({ ...waves });
+const waves = [...generateWaves("[root] root", backwards, forwards)]
+  .map((wave, index) => ({
+    wave,
+    index,
+  }));
+waves.sort((a, b) => b.wave.size - a.wave.size);
+console.log(waves)
